50feca9c798126b9f4f21aa5995a7a6546114966
Omer Khan <omerqavi@gmail.com>
2024-05-29 14:10:15 -0400

    testing login page

--- practice/testdb/models.py
+++ practice/testdb/models.py
@@ -4,3 +4,20 @@ from django.db import models
 class Teacher(models.Model):
     name=models.CharField(max_length=80)
     age= models.IntegerField()
+
+
+
+class Product(models.Model): 
+    name=models.CharField(max_length=50)
+    description=models.CharField(max_length=50)
+    highlight=models.CharField(max_length=50)
+    price = models.IntegerField()
+    image = models.ImageField()
+    rating = models.IntegerField()
+
+
+class Admin (models.Model): 
+    name=models.CharField(max_length=50)
+    email=models.EmailField()
+    password= models.CharField(max_length=20)
+
