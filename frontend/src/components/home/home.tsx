bdfb48e62f58a9f4178c51965323367bfd9534f6
The-Omer-Khan <omerqavi@gmail.com>
2024-06-03 18:57:59 +0500

    home page

--- frontend/src/components/home/home.tsx
+++ frontend/src/components/home/home.tsx
@@ -1,39 +1,76 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Layout, Menu } from 'antd';
-import { Link } from 'react-router-dom';
-import Login from '../login/Login.tsx';
-import Signup from '../signup/signup.tsx';
+import React from "react";
+import { Layout, Menu, Typography, Card, Row, Col } from "antd";
+import { Link } from "react-router-dom";
 
 const { Header, Content } = Layout;
+const { Title, Paragraph } = Typography;
+
+const products = [
+	{
+		id: 1,
+		name: "Product 1",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec convallis fermentum augue.",
+	},
+	{
+		id: 2,
+		name: "Product 2",
+		description:
+			"Nam condimentum efficitur risus, nec iaculis tortor tincidunt sed. Ut in eros at nunc tristique sodales.",
+	},
+	{
+		id: 3,
+		name: "Product 3",
+		description:
+			"Sed id enim vestibulum, bibendum ipsum vel, bibendum lorem. Integer suscipit sapien vitae odio lobortis.",
+	},
+];
 
 function HomePage() {
-  return (
-    <Router>
-      <Layout>
-        <Header>
-          <div className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1"><Link to="/login">Login</Link></Menu.Item>
-            <Menu.Item key="2"><Link to="/signup">Sign Up</Link></Menu.Item>
-          </Menu>
-        </Header>
-        <Content style={{ padding: '0 50px', marginTop: 64 }}>
-          <div className="site-layout-content">
-            {/* Your content goes here */}
-            <h1>Welcome to My Website</h1>
-            <p>This is the homepage content.</p>
-          </div>
-        </Content>
-      </Layout>
-
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Layout>
+			<Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+				<div
+					className="logo"
+					style={{
+						width: 120,
+						height: 31,
+						background: "rgba(255, 255, 255, 0.2)",
+						margin: "16px 24px 16px 0",
+						float: "left",
+					}}
+				/>
+				<Menu
+					theme="dark"
+					mode="horizontal"
+					defaultSelectedKeys={["1"]}
+					style={{ lineHeight: "64px" }}>
+					<Menu.Item key="1">
+						<Link to="/login">Login</Link>
+					</Menu.Item>
+					<Menu.Item key="2">
+						<Link to="/signup">Sign Up</Link>
+					</Menu.Item>
+				</Menu>
+			</Header>
+			<Content style={{ padding: "0 50px", marginTop: 64 }}>
+				<div style={{ background: "#fff", padding: 24, minHeight: 380 }}>
+					<Title level={2}>Welcome to Our Product Page</Title>
+					<Row gutter={[16, 16]}>
+						{products.map(product => (
+							<Col key={product.id} span={8}>
+								<Card>
+									<Title level={3}>{product.name}</Title>
+									<Paragraph>{product.description}</Paragraph>
+									<Link to={`/product/${product.id}`}>View Details</Link>
+								</Card>
+							</Col>
+						))}
+					</Row>
+				</div>
+			</Content>
+		</Layout>
+	);
 }
 
 export default HomePage;
